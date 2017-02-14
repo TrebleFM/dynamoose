81f10bd7f7ee8636534f2b2587a85326a07b9120
Spencer Hakim <spencer@treble.fm>
2017-02-14 16:20:02 -0500

    `eslint —fix` and manually fix a few root files, and removed jshint from gruntfile

--- examples/simple.js
+++ examples/simple.js
@@ -1,34 +1,38 @@
-'use strict';
+"use strict";
 
-var dynamoose = require('../');
-
+const dynamoose = require("../");
 
 /*
-Assumes AWS setting are in environment variables
-
-export AWS_ACCESS_KEY_ID="Your AWS Access Key ID"
-export AWS_SECRET_ACCESS_KEY="Your AWS Secret Access Key"
-export AWS_REGION="us-east-1"
-
-If not, they can be configured via the AWS object.
-
-dynamoose.AWS.config.update({
-  accessKeyId: 'AKID',
-  secretAccessKey: 'SECRET',
-  region: 'us-east-1'
-});
-*/
+ * Assumes AWS setting are in environment variables
+ *
+ * export AWS_ACCESS_KEY_ID="Your AWS Access Key ID"
+ * export AWS_SECRET_ACCESS_KEY="Your AWS Secret Access Key"
+ * export AWS_REGION="us-east-1"
+ *
+ * If not, they can be configured via the AWS object.
+ *
+ * dynamoose.AWS.config.update({
+ *     accessKeyId: 'AKID',
+ *     secretAccessKey: 'SECRET',
+ *     region: 'us-east-1'
+ * });
+ *
+ */
 
 // dynamoose.local(); // Use a local DynamoDB
 
+const Cat = dynamoose.model("Cat", {
+    id: Number,
+    name: String
+});
 
-var Cat = dynamoose.model('Cat', { id: Number, name: String });
-
-var garfield = new Cat({id: 666, name: 'Garfield'});
-
+let garfield = new Cat({
+    id: 1,
+    name: "Garfield"
+});
 garfield.save();
 
-Cat.get(666)
-.then(function (badCat) {
-  console.log('Never trust a smiling cat. - ' + badCat.name);
-});
+Cat.get(1)
+    .then((badCat) => {
+        console.log(`Never trust a smiling cat. - ${badCat.name}`);
+    });
--- gruntfile.js
+++ gruntfile.js
@@ -1,67 +1,49 @@
-'use strict';
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      app: {
-        src: ['gruntfile.js', 'index.js', 'lib/**/*.js'],
-        options: {
-          node: true,
-          jshintrc: '.jshintrc'
+"use strict";
+
+module.exports = (grunt) => {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: "spec"
+                },
+                src: ["test/**/*.js"]
+            },
+            testCoverage: {
+                options: {
+                    reporter: "spec",
+                    require: "test/coverage/blanket"
+                },
+                src: ["test/**/*.js"]
+            },
+            coverage: {
+                options: {
+                    reporter: "html-cov",
+
+                    // use the quiet flag to suppress the mocha console output
+                    quiet: true,
+
+                    // specify a destination file to capture the mocha
+                    // output (the quiet option does not suppress this)
+                    captureFile: "coverage.html"
+                },
+                src: ["test/**/*.js"]
+            },
+            "travis-cov": {
+                options: {
+                    reporter: "travis-cov"
+                },
+                src: ["test/**/*.js"]
+            }
         }
-      },
-      test: {
-        src: ['test/**/*.js' ],
-        options: {
-          node: true,
-          jshintrc: 'test/.jshintrc'
-        }
-      }
-    },
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/**/*.js']
-      },
-      testCoverage: {
-        options: {
-          reporter: 'spec',
-          require: 'test/coverage/blanket'
-        },
-        src: ['test/**/*.js']
-      },
-      coverage: {
-        options: {
-          reporter: 'html-cov',
-          // use the quiet flag to suppress the mocha console output
-          quiet: true,
-          // specify a destination file to capture the mocha
-          // output (the quiet option does not suppress this)
-          captureFile: 'coverage.html'
-        },
-        src: ['test/**/*.js']
-      },
-      'travis-cov': {
-        options: {
-          reporter: 'travis-cov'
-        },
-        src: ['test/**/*.js']
-      }
-    }
-  });
+    });
 
-  // Load libs
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-mocha-test');
+    // Load libs
+    grunt.loadNpmTasks("grunt-mocha-test");
 
-
-  // Register the default tasks
-  grunt.registerTask('default', ['jshint', 'mochaTest']);
-
-  grunt.registerTask('test', ['jshint', 'mochaTest:test']);
-
-  grunt.registerTask('coverage', ['jshint', 'mochaTest:testCoverage', 'mochaTest:coverage', 'mochaTest:travis-cov']);
-
+    // Register the default tasks
+    grunt.registerTask("default", ["mochaTest"]);
+    grunt.registerTask("test", ["mochaTest:test"]);
+    grunt.registerTask("coverage", ["mochaTest:testCoverage", "mochaTest:coverage", "mochaTest:travis-cov"]);
 };
